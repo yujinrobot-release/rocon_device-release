26b987b2a60b80c3f915a1f6ed3bce5457c32d9f
Jihoon Lee <jihoonlee.in@gmail.com>
2015-07-23 16:18:36 +0900

    Trimmed the branch to only the rocon_hue sub directory

--- rocon_device_tools/rocon_python_hue/src/rocon_python_hue/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-#
-# License: BSD
-#   https://raw.github.com/robotics-in-concert/rocon_devices/license/LICENSE
-#
-#################################################################################
-from .phue import *
-from .exceptions import *
